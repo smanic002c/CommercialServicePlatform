605959aa48afc8179fdcd7d8f549f9882bfb9fb8
smanic002c <Sunil_Manickalal2@cable.comcast.com>
2014-08-27 16:41:20 -0400

    initial commit

--- MouseMover/src/com/sources/constants/Constants.java
+++ MouseMover/src/com/sources/constants/Constants.java
@@ -2,6 +2,4 @@ package com.sources.constants;
 
 public class Constants {
 	public static final int SECONDS = 10;
-	public static final String START = "START: ";
-	public static final String END = "END: ";
 }
